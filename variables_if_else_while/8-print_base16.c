f23ee64af995caaa5d272c06cf63cf1c49f87fb2
papadar <aladar@apponyi.net>
2026-07-07 06:27:18 +0000

    program to print one set of hex characters

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: text here
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i < 16; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
