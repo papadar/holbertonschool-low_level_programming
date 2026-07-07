0ca10140a5aae020fc7800e96b36e2c3ca569591
papadar <aladar@apponyi.net>
2026-07-07 00:47:52 +0000

    updated script to match checker

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,8 +3,10 @@ include < stdio.h >
 
 /**
  * main - the required function
+ * return: always return 0
  */
-void main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
