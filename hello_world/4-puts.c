82a3ebb9967f48fe04043f18c350df2a01e92511
papadar <aladar@apponyi.net>
2026-07-07 00:42:15 +0000

    the simplest c program

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+include < stdio.h >
+/* Program to print a line of text only */
+
+/**
+ * main - the required function
+ */
+void main(void)
+{
+	puts("Programming is like building a multilingual puzzle,\n");
+}
