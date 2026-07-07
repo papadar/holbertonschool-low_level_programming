8a29e163a2f6eeeb208c3f48a3bbd94907bdaacd
papadar <aladar@apponyi.net>
2026-07-07 06:02:50 +0000

    Program to print 0-9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: text here
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	int i = '0';
+	int j = '9';
+
+	for (i = '0'; i <= j; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
