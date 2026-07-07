53238bcb52b75b3cda8a415f9e82f5b9f018d2b0
papadar <aladar@apponyi.net>
2026-07-07 06:48:13 +0000

    program to list 0-9 separated by a comma and a space

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: text here
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
