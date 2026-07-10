3407676a24a28c26fdd37ce798f77103dff45c87
papadar <aladar@apponyi.net>
2026-07-10 10:22:35 +0000

    fixed function name

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * islower - determine if received character is lower case
+ * _islower - determine if received character is lower case
  *
  * @c: - c is the value that is received and being checked
  *Return: 0 if not lowercase, 1 if lower case
  */
-int islower(int c)
+int _islower(int c)
 {
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
