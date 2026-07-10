56c063be7f43c257d4f29bf1345e16f7ea3fc961
papadar <aladar@apponyi.net>
2026-07-10 10:35:51 +0000

    removed validilty for 0-9 characters

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,17 +1,13 @@
 #include "main.h"
 
 /**
- * _isalpha - determine if received value is alphanumeric in ascii
+ * _isalpha - determine if received value is a character in ascii
  *
  * @a: - a is the value that is received and being checked
- *Return: 1 if alphanumeric, 0 if not
+ *Return: 1 if alphabetical, 0 if not
  */
 int _isalpha(int a)
 {
-	if (a >= '0' && a <= '9')
-	{
-		return (1);
-	}
 	else if (a >= 'A' && a <= 'Z')
 	{
 		return (1);
