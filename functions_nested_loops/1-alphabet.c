50cc0707621eddd7da95ceaef551d9d186f4f898
papadar <aladar@apponyi.net>
2026-07-10 09:16:38 +0000

    program to print alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ *Description: print the alphabet using only two _printchars
+ *
+ *Return: Always returns 0
+ */
+int main(void)
+{
+	char c;
+	char z;
+
+	c = 'a';
+	z = 'z';
+
+	while (c <= z)
+	}
+		_putchar(c);
+		c++;
+	{
+	_putchar('\n');
+
+	return (0);
+}
