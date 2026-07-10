3b5148b3b0079ed8bf6e2170ef9e37eca02b5055
papadar <aladar@apponyi.net>
2026-07-10 10:15:58 +0000

    program to check if a given character is lower case or not

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * islower - determine if received character is lower case
+ *
+ * @c: - c is the value that is received and being checked
+ *Return: 0 if not lowercase, 1 if lower case
+ */
+int islower(int c)
+{
+	if(c >= 'a' && c <= 'z')
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
