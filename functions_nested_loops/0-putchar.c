e60808c82a02cc849aeba8c9d9aff2e38137779b
papadar <aladar@apponyi.net>
2026-07-10 07:41:13 +0000

    script to print _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Description: text goes here
+ *
+ *Return: Always returns 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+#The Required Readme File
