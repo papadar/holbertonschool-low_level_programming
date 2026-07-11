e9cd58887c89f814b8965a905eacf75506728b65
papadar <aladar@apponyi.net>
2026-07-11 04:37:15 +0000

    program to return the added value of two given integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - returns result of adding two integers
+ *
+ * @f: first integer
+ * @s: second integer
+ *
+ * Return: returns the result of the addition
+ **/
+int add(int f, int s)
+{
+	return (f + s);
+}
