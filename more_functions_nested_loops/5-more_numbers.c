7f923b9519426859600bbf19cea3a275f94afeb7
papadar <aladar@apponyi.net>
2026-07-11 08:06:46 +0000

    updated program to prevent infinite loop

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -16,13 +16,17 @@ void more_numbers(void)
 		n = 0;
 		while (n < 15)
 		{
+			int d;	
+
+			d = (n % 10);
 			if (n > 9)
 			{
 				_putchar('1');
 			}
-			_putchar('0' + n);
+			_putchar('0' + d);
 			n++;
 		}
+		_putchar('\n');
+		t++;
 	}
-	_putchar('\n');
 }
