f1d7f22308f2a7fb86d87922ece35ca01cbc89c6
papadar <aladar@apponyi.net>
2026-07-11 08:00:13 +0000

    program to print numbers 1-14 ten times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers 0-14 ten times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int t;
+	int n;
+
+	t = 0;
+	while (t < 10)
+	{
+		n = 0;
+		while (n < 15)
+		{
+			if (n > 9)
+			{
+				_putchar('1');
+			}
+			_putchar('0' + n);
+			n++;
+		}
+	}
+	_putchar('\n');
+}
