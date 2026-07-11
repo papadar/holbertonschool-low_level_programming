ce81779bb2c33b0b3907442a25506d49eb54d1f9
papadar <aladar@apponyi.net>
2026-07-11 07:11:12 +0000

    program to return result of multipling two provided values

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiply two provided values
+ *
+ * @a: - a is the first value
+ * @b: - b is the second value
+ *
+ * Return: result of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
