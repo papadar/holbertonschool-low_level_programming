19d85ab685ba1c99212da47a046135283440e13e
papadar <aladar@apponyi.net>
2026-07-12 03:00:28 +0000

    fizz buzz program

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - print a sequence 0 - 100 replacing each
+ * factor of 3 with a Fizz & each factor of 5 with a Buzz
+ *
+ * Return: always returns 0
+ */
+int main(void)
+{
+	int count;
+
+	count = 1;
+	while (count <= 100)
+	{
+		if (count % 3 == 0)
+		{
+			if (count % 5 == 0)
+			{
+				printf("FizzBuzz");
+			}
+			else
+			{
+				printf("Fizz");
+			}
+		}
+		else if (count % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", count);
+		}
+		putchar(' ');
+		count++;
+	}
+	putchar('\n');
+	return (0);
+}
