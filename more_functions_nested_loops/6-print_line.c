e881ba0738ed2023d2519c0be682302c3bdef1ae
papadar <aladar@apponyi.net>
2026-07-12 02:17:09 +0000

    program to print underscore n times

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - print the underscore character n times
+ *
+ * @n - received value equals number of times to print '_'
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
