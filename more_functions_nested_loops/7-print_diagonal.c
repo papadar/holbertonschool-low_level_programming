9c1226dcdc609ba9b0a86e3b17fed5339ce2a05c
papadar <aladar@apponyi.net>
2026-07-12 02:32:00 +0000

    program to print a diagonal line

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print a new line and slash n times
+ *
+ * @n: - received value
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int m;
+
+	m = n;
+	while (n > 0)
+	{
+		int s;
+
+		s = m - n;
+		while (s > 0)
+		{
+			_putchar(' ');
+			s--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		n--;
+	}
+}
