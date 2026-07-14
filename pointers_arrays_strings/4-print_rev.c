edc5f98115996d981b4f78e809170d1aa4669546
papadar <aladar@apponyi.net>
2026-07-14 03:44:06 +0000

    program to print a reversed string

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a reversed string and a new line
+ *
+ * @s: received pointer to string
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int c;
+
+	c = 0;
+	while (str[c] != '\0')
+		c++;
+	while (c > 0)
+	{
+		_putchar(str[c]);
+		c--;
+	}
+	_putchar('\n');
+}
