088f8110ba0de021d1c6148bd377b982bfba7fd1
papadar <aladar@apponyi.net>
2026-07-14 00:39:33 +0000

    program to reset a pointed value to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - change the value of the pointed int to 98
+ *
+ * @n: this is the pointer that is received
+ *
+ * Return: void no return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
