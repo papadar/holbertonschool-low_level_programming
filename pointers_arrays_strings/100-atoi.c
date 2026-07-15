6cdde591e454205c7accbbcec83caa8a54830b2e
papadar <aladar@apponyi.net>
2026-07-15 04:11:22 +0000

    tweak to handle multiple negative symbols

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,26 +11,24 @@ int _atoi(char *s)
 {
 	int c, v, b;
 
-	c = v = b = 0;
+	c = v = 0;
+	b = 1;
 	while (s[c] != '\0')
 	{
 		if (s[c] == '-')
-			b = -1;
+			b *= -1;
 		else if (s[c] >= '0' && s[c] <= '9')
 			v = (v * 10) + (s[c] - '0');
 		else if (s[c] < '0' || s[c] > '9')
 		{
-			if (v == 0)
-				b = 1;
-			else if (b < 0)
+			if (v > 0)
 			{
-				v = (v * -1);
+				v = (v * b);
 				return (v);
 			}
 		}
 		c++;
 	}
-	if (b < 0)
-		v = (v * -1);
+	v = (v * b);
 	return (v);
 }
