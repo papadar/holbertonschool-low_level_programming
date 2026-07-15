a49c73372f314171dd3332714f0fb7c263f25c4c
papadar <aladar@apponyi.net>
2026-07-15 10:09:46 +0000

    program to copy n bytes of a string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies n bytes of a string
+ *
+ * @dest: destination pointer received
+ * @src: source pointer received
+ * @n: number of bytes to quit at
+ *
+ * Return: a point to a new string that is
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int c;
+
+	c = 0;
+	while (src[c] != '\0')
+	{
+		if (c < n)
+			dest[c] = src[c];
+		else
+			dest[c] = '\0';
+		c++;
+	}
+	dest[c] = '\0';
+	return (dest);
+}
