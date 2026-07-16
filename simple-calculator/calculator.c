0aae8811adb0655bc21ab0e728b28ecc6f051f70
papadar <aladar@apponyi.net>
2026-07-16 03:26:24 +0000

    work in progress on calculatopr

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -12,7 +12,6 @@ void pr_menu(int m)
 	if (m == 0)
 	{
         	/* print the main menu*/
-		printf("Simple Calculator\n");
 		printf("1) Add\n");
 		printf("2) Subtract\n");
 		printf("3) Multiply\n");
@@ -30,11 +29,38 @@ int main(void)
 {
 	int input;
 
+	input = -999;
+
+	printf("Simple Calculator\n");
 	pr_menu(0);
-
-	scanf("%d", &input);
-
-	printf("Choice: %d\n", input);
+	while (input != 0)
+	{
+		scanf("%d", &input);
+		if (input < 0 || input > 4)
+			printf("Invalid choice\n");
+		else
+		{
+			printf("Choice: %d\n", input);
+			switch (input)
+			{
+				case 0: 
+					printf("exit: %d\n", input);
+					break;
+				case 1: 
+					printf("one: %d\n", input);
+					break;
+				case 2: 
+					printf("two: %d\n", input);
+					break;
+				case 3: 
+					printf("three: %d\n", input);
+					break;
+				case 4: 
+					printf("four: %d\n", input);
+					break;
+			}
+		}
+	}
 	printf("Bye!\n");
 
 	return (0);
