297f8705efa73b10a1b867b8bcfd0b74db4a042c
papadar <aladar@apponyi.net>
2026-07-18 07:06:51 +0000

    program to compare two strings and return a likeness value

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,15 +10,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-    int c;
+	int c;
 
 	c = 0;
 	while (s1[c] != '\0' || s2[c] != '\0')
 	{
-        if (s1[c] < s2[c])
-            return -15;
-        if (s1[c] > s2[c])
-            return 15;
+		if (s1[c] < s2[c])
+			return (-15);
+		if (s1[c] > s2[c])
+			return (15);
 		c++;
 	}
 
