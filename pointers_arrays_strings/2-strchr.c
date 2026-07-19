a8674c5035537227c26f8d42a2f6206885e37c8e
papadar <aladar@apponyi.net>
2026-07-19 06:53:12 +0000

    program to return the position within a string of a character

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - find a given character in a string
+ *
+ * @s: pointer to a string
+ * @c: character to find
+ *
+ * Return: pointer to the first character or null
+ */
+char *_strchr(char *s, char c)
+{
+	int n;
+
+	n = 0;
+	while (s[n] != '\n')
+	{
+		if (s[n] == c)
+			return (&s[n]);
+		n++;
+	}
+	return ('\0');
+}
