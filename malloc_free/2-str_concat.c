1903b831a5713315412c2ebca1511be3fc66c8c8
papadar <aladar@apponyi.net>
2026-07-21 08:26:38 +0000

    improved null string handling

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -32,30 +32,30 @@ char *str_concat(char *s1, char *s2)
 	unsigned int c0, c1, c2;
 
 	c0 = 0;
-
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
 	c1 = str_count(s1);
 	c2 = str_count(s2);
-
-	printf("count c1: %d\n", c1);
-	printf("count c2: %d\n", c2);
-
 	ar = (char *)malloc((c1 + c2 + 1) * sizeof(char));
 	if (ar)
 	{
-		while (c0 < c1)
+		if (c1 > 0)
 		{
-			ar[c0] = s1[c0];
-			c0++;
+			while (c0 < c1)
+			{
+				ar[c0] = s1[c0];
+				c0++;
+			}
 		}
-		while (c0 < (c1 + c2))
+		if (c2 > 0)
 		{
-			ar[c0] = s2[c0 - c1];
-			c0++;
+			while (c0 < (c1 + c2))
+			{
+				ar[c0] = s2[c0 - c1];
+				c0++;
+			}
 		}
 		ar[c0] = '\0';
 		return (ar);
