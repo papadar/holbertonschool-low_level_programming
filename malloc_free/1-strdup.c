7fb3448f1e129166e86667dba0a25eabaf3e4d05
papadar <aladar@apponyi.net>
2026-07-21 01:44:40 +0000

    program to duplicate a string

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - duplicate a string
+ *
+ * @str: the given pointer to a string
+ *
+ * Return: pointer to the first char of the duped string
+ */
+char *_strdup(char *str)
+{
+	char *ar;
+	unsigned int len, c;
+
+	len = c = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	if (len > 0)
+	{
+		ar = (char *)malloc((len + 1) * sizeof(char));
+
+		if (ar)
+		{
+			while (c < len + 1)
+			{
+				ar[c] = str[c];
+				c++;
+			}
+			return (&ar[0]);
+		}
+		else
+		{
+			free(ar);
+			return (NULL);
+		}
+	}
+	else
+		return (NULL);
+}
