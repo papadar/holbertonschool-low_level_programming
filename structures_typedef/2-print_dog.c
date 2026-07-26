d9ab1e1c38162a850a5a593b15706c04e505d520
papadar <aladar@apponyi.net>
2026-07-26 07:14:37 +0000

    updating program to handle null pointers

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,10 +12,20 @@
 
 void print_dog(struct dog *d)
 {
+	
+
 	if (d)
 	{
-		printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+
+		printf("Age: %f\n", 0.0);
+
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
 	}
 }
