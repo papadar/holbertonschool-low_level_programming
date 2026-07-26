4099da85f65407b24675110224b38abb12e205bb
papadar <aladar@apponyi.net>
2026-07-26 07:18:35 +0000

    re-instating the correct line

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,8 +12,6 @@
 
 void print_dog(struct dog *d)
 {
-	
-
 	if (d)
 	{
 		if (d->name == NULL)
@@ -21,7 +19,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %s\n", d->name);
 
-		printf("Age: %f\n", 0.0);
+		printf("Age: %f\n", d->age);
 
 		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
