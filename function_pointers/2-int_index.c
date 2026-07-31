f9ad99a9b71b4f350d35f5012392d1d83a25f418
papadar <aladar@apponyi.net>
2026-07-31 07:07:10 +0000

    program to find a matching index within an array

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * int_index - return the index of a value within an array
+ *
+ * @array: pointer to an array
+ * @size: size of the array
+ * @cmp: pointer to the comparison function
+ *
+ * Return: - integer - index within the array
+ * -1 if not found / invalid request
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int count;
+
+	if (array && size > 0)
+	{
+		count = 0;
+		while (count < size)
+		{
+			if (cmp(array[count]) != 0)
+				return (count);
+			count++;
+		}
+		return (-1);
+	}
+	else
+		return (-1);
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,7 +1,10 @@
+#include <stddef.h>
+
 #ifndef MY_STRUCT_H
 #define MY_STRUCT_H
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
