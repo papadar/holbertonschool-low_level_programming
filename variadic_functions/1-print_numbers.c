8fb40483e21a837569931d4211ab76d85a3a5ac1
papadar <aladar@apponyi.net>
2026-08-01 08:17:07 +0000

    better handling of NULL separator

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	i = 0;
 	va_start(List, n);
-	if (n > 0 && separator != NULL)
+	if (n > 0)
 	{
 		if (separator == NULL)
 			separator = "";
