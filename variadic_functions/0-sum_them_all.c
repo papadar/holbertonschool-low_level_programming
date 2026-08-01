ddf179adf3892b6c256d7d0eaf0e2910dee3b4ed
papadar <aladar@apponyi.net>
2026-08-01 07:52:29 +0000

    function to sum a number of intswq

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,1 +1,24 @@
+#include <stdarg.h>
 #include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums them all
+ * @n: number of them
+ * Return: int sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list List;
+	int total;
+	unsigned int i;
+
+	va_start(List, n);
+	total = i = 0;
+	while (i < n)
+	{
+		total += va_arg(List, int);
+		i++;
+	}
+	va_end(List);
+	return (total);
+}
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef _MYDEF_H
+#define _MYDEF_H
+
+int _putchar(char c);
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
