cb1bef099f3d01ba56abdd027be8c754be4d9eb1
papadar <aladar@apponyi.net>
2026-08-02 16:15:31 +1000

    progress on calculator

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,24 +1,46 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
 
 /**
- * main - the calculator program
- *
- * Return: always retutns 0
+ * main - the calculator main
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, anything else is an error
  */
 
-int main(char *str)
+int main(int argc, char *argv[])
 {
-
 	int a, b, count;
 
 	a = b = count = 0;
-	if (str)
-		a = atoi(str);
 
-
-	atoi
 	/*get the input*/
 	/*analyse the input*/
 	/*handle the input*/
-	return (1);
+
+	if (argc == 4)
+	{
+		switch (argv[2])
+			case '+':
+				printf("add");
+				break;
+			case '-':
+				printf("sub");
+				break;
+			case '*':
+				printf("mult");
+				break;
+			case '/':
+				printf("div");
+				break;
+			case '%':
+				printf("mod");
+				break;
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 }
