f7a6fd2c2c1e8e3ad07606aa27237e64169cf7a4
papadar <aladar@apponyi.net>
2026-08-09 06:21:24 +0000

    program to print a linked list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - prints a value from the list struct
+ * @h: pointer to the first item of the list
+ * Return: the size of the linked list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t size = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] mil\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		size++;
+		h = h->next;
+	}
+
+	return (size);
+}
