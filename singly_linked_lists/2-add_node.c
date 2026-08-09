87df03a78051d91197f2a364b8f36afd37b22f79
papadar <aladar@apponyi.net>
2026-08-09 07:13:09 +0000

    program to add a node to a linked list

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_node - add a node to the list, and tidy up pointers
+ * @head: pointer to the first item of the list
+ * @str: pointer to the string of text
+ * Return: pointer to the new item
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	unsigned int strlen = 0;
+	list_t *temp = malloc(sizeof(list_t));
+
+	if (!temp)
+		return (NULL);
+
+	while (str[strlen] != '\0')
+		strlen++;
+	if (strlen == 0)
+		return (NULL);
+
+	temp->str = strdup(str);
+	temp->len = strlen;
+	temp->next = *head;
+	*head = temp;
+	return (temp);
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define MYDEFINE_H
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 /**
