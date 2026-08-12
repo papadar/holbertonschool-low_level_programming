79c9f5a2ae0e0927053effbca789409dc56adfe3
papadar <aladar@apponyi.net>
2026-08-12 04:48:40 +0000

    program to add an item to the list

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a new element to the dub_list
+ * @head: **pointer to the head item
+ * @n: the integer to store
+ * Return: pointer to the new item
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *temp = malloc(sizeof(dlistint_t));
+
+	if (!temp)
+		return (NULL);
+	temp->n = n;
+	temp->next = *head;
+	temp->prev = NULL;
+	*head = temp;
+	return (temp);
+}
