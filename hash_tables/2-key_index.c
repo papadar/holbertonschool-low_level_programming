60f7b990b992f60fd4dfaf9118aa4fb598a97c8d
papadar <aladar@apponyi.net>
2026-08-15 03:37:09 +0000

    program to find an array index from a string hash

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,1 +1,18 @@
 #include "hash_tables.h"
+
+/**
+ * key_index - get an index within the array size by hashing the key
+ * @key: the string to hash
+ * @size: size of the array
+ * Return: the index within the array or NULL
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index = hash_djb2(key);
+
+	while (index > size)
+		index = (index % size);
+
+	return (index);
+}
