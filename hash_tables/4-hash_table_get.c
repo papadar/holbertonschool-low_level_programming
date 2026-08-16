149d63dbb3983f3e6c6776e665bde4f40ca94231
papadar <aladar@apponyi.net>
2026-08-16 02:40:40 +0000

    program to retrieve a value when provided a key

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,2 +1,26 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_get - retreives the value matching the provided key
+ * @ht: pointer to the array
+ * @key: string
+ * Return: pointer to the value or NULL
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *head;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	head = ht->array[index];
+	if (head == NULL)
+		return (NULL);
+	while (strcmp(key, head->key) != 0)
+		head = head->next;
+	if (head == NULL)
+		return (NULL);
+	return (head->value);
+}
