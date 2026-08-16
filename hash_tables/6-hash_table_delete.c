ed81a31c8fc5ab1c32d23db69d9027d6ab3e0d34
papadar <aladar@apponyi.net>
2026-08-16 03:29:58 +0000

    program to delete a hash table

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,2 +1,31 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_delete - delete an entire hash table & free the memory
+ * @ht: pointer to the table struct
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *head;
+	hash_node_t *temp;
+
+	i = 0;
+	while (i < ht->size)
+	{
+		head = ht->array[i];
+		while (head)
+		{
+			temp = head->next;
+			free(head->key);
+			free(head->value);
+			free(head);
+			head = temp;
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
+}
