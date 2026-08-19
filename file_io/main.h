ebb1d4f5091425dcb37e1712d2fb8e3e26e91344
papadar <aladar@apponyi.net>
2026-08-19 09:51:53 +0000

    added required project files

---
+++ file_io/main.h
@@ -0,0 +1,9 @@
+#ifndef MYDEFINE_H
+#define MYDEFINE_H
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+
+#endif
