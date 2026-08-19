e7c22228960c27a92973202be35b237ae160c8ed
papadar <aladar@apponyi.net>
2026-08-19 04:56:36 +0000

    program that recursively tallies the length of a string

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - recursively count the length of a string
+ * @s: pointer to char 0
+ * Return: number of chars
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
