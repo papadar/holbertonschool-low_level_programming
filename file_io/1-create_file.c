2eea0140de589cac9e9111ef9fc177105fe6baa4
papadar <aladar@apponyi.net>
2026-08-21 06:24:13 +0000

    updated handling of null string

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,12 +14,10 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	count = file = out = 0;
+	if (!text_content)
+		text_content = "";
 	while (text_content[count] != '\0')
 		count++;
-
-	if (count == 0)
-		text_content = "";
-
 	file = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (file == -1)
 		return (-1);
